9a48c556ce7b3b9ffb8b66881d5bb229e395d84a
Paweł Szczerbiak <p.szczerbiak@gmail.com>
2023-08-03 11:35:13 +0200

    add plugin skeleton

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2023, Bioinformatics at Małopolska Centre of Biotechnology
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name="q2-dynamo",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    author="Zuzanna Karwowska",
+    author_email="zuzanna.karwowska@doctoral.uj.edu.pl",
+    description="",
+    license='BSD-3-Clause',
+    url="https://github.com/bioinf-mcb/q2-dynamo",
+    entry_points={
+        'qiime2.plugins':
+        ['q2-dynamo=q2_dynamo.plugin_setup:plugin']
+    },
+    package_data={
+    },
+    zip_safe=False,
+)
